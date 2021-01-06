f1d92b944153df7978d9d6141e899bb627ee1919
Dmitry Kyrychenko <kirichenkodmitryy@gmail.com>
2021-01-06 23:44:48 +0300

    Refactoring

--- MagicPaper/ViewController.swift
+++ MagicPaper/ViewController.swift
@@ -10,8 +10,11 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
+    
+    private var isTouch: Bool = false
+    private var videoNode = SKVideoNode()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,19 +25,20 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
+        let configuration = ARImageTrackingConfiguration()
+        
+        //Track images within this bundle
+        if let trackedImages = ARReferenceImage.referenceImages(inGroupNamed: "NewsPaperImages", bundle: Bundle.main) {
+            configuration.trackingImages = trackedImages
+            configuration.maximumNumberOfTrackedImages = 1
+        }
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -45,30 +49,42 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
+    
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
-     
+        
+        guard let imageAnchor = anchor as? ARImageAnchor else { return nil}
+        guard let videoName = imageAnchor.referenceImage.name else { return nil}
+        
+        videoNode = SKVideoNode(fileNamed: "\(videoName).mp4")
+        videoNode.play()
+        
+        // Display Spritekit element via SceneKit
+        let videoScene = SKScene(size: CGSize(width: 480, height: 360))
+        videoNode.position = CGPoint(x: videoScene.size.width / 2, y: videoScene.size.height / 2)
+        videoNode.yScale = -1.0
+        videoScene.addChild(videoNode)
+        
+        let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+        plane.firstMaterial?.diffuse.contents = videoScene
+        
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.eulerAngles.x = -.pi / 2
+        node.addChildNode(planeNode)
+        
         return node
     }
-*/
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if isTouch == false{
+            videoNode.play()
+            isTouch = true
+        }
+        else {
+            videoNode.pause()
+            isTouch = false
+        }
     }
 }
